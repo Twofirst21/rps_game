8f77c41c5740b912556e21b2903c1347c04af0bb
Twofirst21 <Twofirst21@gmail.cm>
2015-12-10 20:19:59 -0500

    unknown

---
+++ js/function.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    var myWords = ["Rock", "Paper", "Scissors"];
+
+    $("#startBtn").on("click", function(){
+        var myRandomNumber = Math.floor(Math.random() * myWords.length);
+        $("h1").fadeIn(250, function() {
+            $(this).html(myWords[myRandomNumber]);
+            $(this).fadeOut(1000);
+        });
+    });
+});
